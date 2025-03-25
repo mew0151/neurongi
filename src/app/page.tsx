88c30dca62db28fc7e0564e5763cb46f8c4fbc05
cama323@naver.com <cama323@naver.com>
2025-03-25 19:11:29 +0900

    ㄴㅊㅌㅅㅌ2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { usePWA } from "@/hooks/usePWA";
+// import { usePWA } from "@/hooks/usePWA";
 import "@/styles/pages/home.scss";
 
 // components
-import WebLanding from "@/components/home/WebLanding";
+// import WebLanding from "@/components/home/WebLanding";
 import AppMain from "@/components/home/AppMain";
 
 export default function Home() {
-    const isPWA = usePWA();
-    return <div className="home">{isPWA ? <AppMain /> : <WebLanding />}</div>;
+    // const isPWA = usePWA();
+    return (
+        <div className="home">
+            {/* {isPWA ? <AppMain /> : <WebLanding />} */}
+            <AppMain />
+        </div>
+    );
 }
